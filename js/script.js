2b861a6279a3452eb5bb28a12c6acd9fa12dd9ab
RKostochko <kostochko.tech@mail.ru>
2021-08-16 21:34:34 +0300

    fead: create image slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const date = new Date();
 const hours = date.getHours();
+
 const input = document.querySelector('.name');
+const greeting = document.querySelector('.greeting');
+
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
+let randomNum;
 
 function showTime() {
 	const date = new Date();
@@ -33,7 +40,7 @@ function getTimeOfDay() {
 function showGreeting() {
 	const timeOfDay = getTimeOfDay();
 	const greetingText = `Good ${timeOfDay}`;
-	document.querySelector('.greeting').innerHTML = greetingText;
+	greeting.innerHTML = greetingText;
 }
 
 function setLocalStorage() {
@@ -47,3 +54,39 @@ function getLocalStorage() {
 	}
 }
 window.addEventListener('load', getLocalStorage);
+
+function getNumberNow(min, max){
+	min = 1;
+	max = 20;
+	randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setBg(){
+	const timeOfDay = getTimeOfDay();
+	const bgNum = getNumberNow().toString().padStart(2, "0");
+
+	document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+}
+setBg();
+
+function getSlideNext() {
+	if (randomNum === 20){
+		randomNum = 01;
+	} else {
+		randomNum = randomNum + 1;
+	}
+	
+	setBg();
+}
+slideNext.addEventListener('click', getSlideNext);
+
+function getSlidePrev() {
+	if (randomNum === 01) {
+		randomNum = 20;
+	} else {
+		randomNum = randomNum - 1;
+	}
+	setBg();
+}
+slidePrev.addEventListener('click', getSlidePrev);
