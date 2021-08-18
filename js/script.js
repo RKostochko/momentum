2d2c4f816bf0075be6da2477841604aa430be8ee
RKostochko <kostochko.tech@mail.ru>
2021-08-18 17:41:55 +0300

    refactor: delete player

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ function getSlidePrev() {
 slidePrev.addEventListener('click', getSlidePrev);
 
 async function getWeather() {
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
 	const res = await fetch(url);
 	const data = await res.json();
 
@@ -113,6 +113,6 @@ function setCity(event) {
 		city.blur();
 	}
 }
-
 document.addEventListener('DOMContentLoaded', getWeather);
 city.addEventListener('keypress', setCity);
+
